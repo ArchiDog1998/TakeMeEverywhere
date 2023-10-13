d9e9744819c2e70e8896dbba37b2cf0132be87e4
秋水 <1123993881@qq.com>
2023-10-13 11:40:19 +0800

    feat: add submodules.

---
+++ TakeMeEverywhere/TakeMeEverywherePlugin.cs
@@ -0,0 +1,23 @@
+﻿using Dalamud.Plugin;
+using ECommons;
+
+namespace TakeMeEverywhere;
+
+public sealed class TakeMeEverywherePlugin : IDalamudPlugin, IDisposable
+{
+    XIVPainter.XIVPainter _painter;
+    XIVRunner.XIVRunner _runner;
+    public TakeMeEverywherePlugin(DalamudPluginInterface pluginInterface)
+    {
+        ECommonsMain.Init(pluginInterface, this, Module.DalamudReflector, Module.ObjectFunctions);
+        _painter = XIVPainter.XIVPainter.Create(pluginInterface, "Take Me Everywhere");
+        _runner = XIVRunner.XIVRunner.Create(pluginInterface);
+    }
+
+    public void Dispose()
+    {
+        _painter.Dispose();
+        _runner.Dispose();
+        ECommonsMain.Dispose();
+    }
+}
